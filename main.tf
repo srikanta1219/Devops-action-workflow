0d09a65201321df448c3d521ebb13b225d7d35a1
srikanta1219 <82170893+srikanta1219@users.noreply.github.com>
2022-09-23 23:11:53 +0530

    Lambda Creation

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = var.region #"us-east-1"
+  region     = "us-east-1"
   access_key = "AKIA35DWZRKVHI76LRPJ"
   secret_key = "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"
 }
