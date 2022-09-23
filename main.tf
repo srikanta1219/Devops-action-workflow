49cf084699ead91b425999f021fadbdc0ac5635c
srikanta1219 <82170893+srikanta1219@users.noreply.github.com>
2022-09-23 23:20:21 +0530

    Delete SQS

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
   secret_key = "LaVQtiHxHHJw1rjKxTX/YOHsgjwh7YC+6C4ORvgh"
 }
 ###############################
-resource "aws_sqs_queue" "queue" {
-  name                      = "sn-apigateway-queue"
-  delay_seconds             = 0
-  max_message_size          = 262144
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  visibility_timeout_seconds = 60
+# resource "aws_sqs_queue" "queue" {
+#   name                      = "sn-apigateway-queue"
+#   delay_seconds             = 0
+#   max_message_size          = 262144
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
+#   visibility_timeout_seconds = 60
 
 
-}
+# }
