1a447ed29410fee3bb432990c1b9008d5957deff
srikanta1219 <82170893+srikanta1219@users.noreply.github.com>
2022-09-23 23:18:51 +0530

    Create SQS

--- main.tf
+++ main.tf
@@ -4,44 +4,13 @@ provider "aws" {
   secret_key = "LaVQtiHxHHJw1rjKxTX/YOHsgjwh7YC+6C4ORvgh"
 }
 ###############################
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name = "iam_for_lambda"
+resource "aws_sqs_queue" "queue" {
+  name                      = "sn-apigateway-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  visibility_timeout_seconds = 60
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
 
-resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
- # filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
-#   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
-
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  #source_code_hash = filebase64sha256("lambda_function_payload.zip")
-
-  runtime = "nodejs12.x"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
 }
